ffece25511c8a3dad45fdb5b4f863acaac5e304f
Malin <szyszka45@gmail.com>
2019-05-03 23:42:14 +0100

    'fixes'

--- main.py
+++ main.py
@@ -23,8 +23,25 @@ data = dataset['ints'][:]
 #data = data.reshape(-1, img_shape[0], img_shape[1], img_shape[2])
 dataset.close()
 
+def save_imgs(self, epoch):
+    r, c = 2, 2
+    noise = np.random.normal(0, 1, (r * c, latent_dim))
+    gen_imgs = self.generator.predict(noise)
+    # Rescale images 0 - 1
+    gen_imgs = 0.5 * gen_imgs + 0.5
+
+    fig, axs = plt.subplots(r, c)
+    cnt = 0
+    for i in range(r):
+        for j in range(c):
+            axs[i, j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+            axs[i, j].axis('off')
+            cnt += 1
+    fig.savefig(f"outputs/output_{str(epoch).zfill(5)}.png")
+    plt.close()
+
 generator = mnist_generator(latent_dim, channels)
 disc = mnist_discriminator(img_shape)
 trainer = Trainer(data, generator, disc, latent_dim, img_shape)
 trainer.multithreaded = False
-trainer.train(epochs=100001, batch_size=64, save_interval=25)
+trainer.train(epochs=100001, batch_size=64, save_interval=25, interval_function=save_imgs)
